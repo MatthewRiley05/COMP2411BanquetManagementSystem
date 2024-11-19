edcc35b94d66aed2202afceb32afcd5549c03bb0
AshVXmc <ashtonsamuel2121@gmail.com>
2024-11-19 17:30:18 +0800

    refactor: created isValidDate util function

--- dbms_cli/commands.py
+++ dbms_cli/commands.py
@@ -1,19 +1,23 @@
 import datetime
 import sqlite3
 
-def createNewBanquet(id : int, name : str, date_time : str, address : str, location : str, quota : int, available : int, first_name : str, last_name : str, remarks, cursor):
-    try:
-        datetime.date.fromisoformat(datetime)
-    except ValueError:
-        raise ValueError("Incorrect date format, should be 'YYYY-MM-DD")
-    
+def createNewBanquet(id : int, name : str, date : str, address : str, location : str, quota : int, available : int, first_name : str, last_name : str, remarks, cursor):
     try:
         sqliteConnection = sqlite3.connect('banquetDatabase.db')
         cursor = sqliteConnection.cursor()
-        cursor.execute("INSERT INTO Banquet VALUES ({id}, {name}, {date_time}, {address}, {location}, {quota}, {available}, {first_name}, {last_name}, {remarks})")
+        cursor.execute("INSERT INTO Banquet VALUES ({id}, {name}, {date}, {address}, {location}, {quota}, {available}, {first_name}, {last_name}, {remarks})")
         print("Created a new banquet. ID: {id}, Name: {name}")
     except Exception:
-        if (None in (id, name, date_time, address, location, quota, available, first_name, last_name)):
+        if (None in (id, name, date, address, location, quota, available, first_name, last_name)):
             print("Value of all arguments (except remarks) must not be None .")
         if (available != 0 or 1):
             print("Value of 'available' must be 0 (false) or 1 (true).")
+        if (isValidDate(date)):
+            print("Incorrect format for date. Correct format: YYYY-MM-DD")
+
+# util functions
+def isValidDate(date : str) -> bool:
+    year = date[0:4]
+    month = date[5:7]
+    day = date[8:]
+    return date == 10 and date[4] == '-' and date[7] == '-' and year.isnumeric() and month.isnumeric() and day.isnumeric()
