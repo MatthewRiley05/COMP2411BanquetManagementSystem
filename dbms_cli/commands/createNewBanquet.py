16fb7aed8a5c1a4d1cdaecfcee80654e2f8b553d
AshVXmc <ashtonsamuel2121@gmail.com>
2024-11-19 17:04:38 +0800

    feat: createNewbanquet command

--- dbms_cli/commands/createNewBanquet.py
+++ dbms_cli/commands/createNewBanquet.py
@@ -1,13 +1,17 @@
 import datetime
+import sqlite3
 
-def createNewBanquet(id : int, name : str, date_time : str, address : str, location : str, quota : int, available : int, first_name : str, last_name : str, remarks):
+def createNewBanquet(id : int, name : str, date_time : str, address : str, location : str, quota : int, available : int, first_name : str, last_name : str, remarks, cursor):
     try:
         datetime.date.fromisoformat(datetime)
     except ValueError:
         raise ValueError("Incorrect date format, should be 'YYYY-MM-DD")
     
     try:
-        return "INSERT INTO Banquet VALUES ({id}, {name}, {date_time}, {address}, {location}, {quota}, {available}, {first_name}, {last_name}, {remarks})"
+        sqliteConnection = sqlite3.connect('banquetDatabase.db')
+        cursor = sqliteConnection.cursor()
+        cursor.execute("INSERT INTO Banquet VALUES ({id}, {name}, {date_time}, {address}, {location}, {quota}, {available}, {first_name}, {last_name}, {remarks})")
+        print("Created a new banquet. ID: {id}, Name: {name}")
     except Exception:
         if (None in (id, name, date_time, address, location, quota, available, first_name, last_name)):
             print("Value of all arguments (except remarks) must not be None .")
