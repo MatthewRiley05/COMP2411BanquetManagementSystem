129141ff80df4a84984e92aecbf2f85eb6612c02
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2024-11-19 15:36:04 +0800

    feat: Enhance database connection handling and add SQLite version check

--- dbms_cli/main.py
+++ dbms_cli/main.py
@@ -1,8 +1,24 @@
 import sqlite3
-
-try :
+ 
+try:
+   
     sqliteConnection = sqlite3.connect('banquetDatabase.db')
-    print("Database connected successfully")
-    
+    cursor = sqliteConnection.cursor()
+    print('DB Init')
+ 
+    query = 'select sqlite_version();'
+    cursor.execute(query)
+ 
+    result = cursor.fetchall()
+    print('SQLite Version is {}'.format(result))
+ 
+    cursor.close()
+ 
 except sqlite3.Error as error:
-    print("Error while connecting to sqlite", error)
+    print('Error occurred - ', error)
+ 
+finally:
+   
+    if sqliteConnection:
+        sqliteConnection.close()
+        print('SQLite Connection closed')
